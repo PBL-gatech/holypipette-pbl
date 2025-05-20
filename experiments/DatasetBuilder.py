dd16f6e0adaca741a350200b3e5ec980db587baa
PBL-gatech <167556129+PBL-gatech@users.noreply.github.com>
2025-05-20 17:40:13 -0400

    v16
    added more random positions
    
    Co-Authored-By: Benjamin Magondu <135537119+bmagondu3@users.noreply.github.com>

--- experiments/DatasetBuilder.py
+++ experiments/DatasetBuilder.py
@@ -1094,10 +1094,24 @@ if __name__ == '__main__':
 
     # rig_recorder_data_folder_set =  ["2025_03_11-16_32"] # inference test data (3/11/2025)
         
-
+    rig_recorder_data_folder_set =  [
+        "2025_05_20-15_50",
+        "2025_05_20-15_16",
+        "2025_05_20-14_05",
+        "2025_04_10-11_57",
+        "2025_04_10-12_16",
+        "2025_04_10-12_21",
+        "2025_04_10-12_30",
+        "2025_04_10-15_01",
+        "2025_04_10-17_31",
+        "2025_04_07-14_32", 
+        "2025_04_07-14_50", 
+        "2025_04_07-15_50", 
+        "2025_04_07-18_04"
+     ] # completely manual HEK data with no overlays. (5/20/2025) v16, including more random start positions
     datasetBuilder = DatasetBuilder(
         dataset_name=dataset_name,
-        val_ratio=1/6,              # 1-in-6 validation demos
+        val_ratio=0,              # 1-in-6 validation demos
         omit_stage_movement=False,   # skip demos with stage XYZ motion
         random_seed=0               # change to alter the split
     )
