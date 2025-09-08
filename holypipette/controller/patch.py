a93912e4927358c983073b3683894c83b5c7414c
PBL-gatech <167556129+PBL-gatech@users.noreply.github.com>
2025-09-08 11:49:53 -0400

    Update patch.py
    temporarily disable config specific commands
    
    Co-Authored-By: Benjamin Magondu <135537119+bmagondu3@users.noreply.github.com>

--- holypipette/controller/patch.py
+++ holypipette/controller/patch.py
@@ -259,10 +259,10 @@ class AutoPatcher(TaskController):
         
         # move to cell position 
         cell_pos, cell_img,pos = cell
-        if self.config.cell_type == "Plate":
-            self.config.cell_distance = 20
-        elif self.config.cell_type == "Slice":
-            self.config.cell_distance = 75
+        # if self.config.cell_type == "Plate":
+        #     self.config.cell_distance = 20
+        # elif self.config.cell_type == "Slice":
+        #     self.config.cell_distance = 75
             
 
         self.info(f" Moving to Cell position: {cell_pos}") 
@@ -321,17 +321,17 @@ class AutoPatcher(TaskController):
         zdistleft  = z_pos - cell_pos[2]
         self.microscope.relative_move(-zdistleft)
         self.microscope.wait_until_still()
-        if self.config.cell_type == "Plate": 
-            self.info("centering on cell")
-            self.calibrated_stage.center_on_cell(cell)
-            self.calibrated_stage.wait_until_still()
-            self.info(f"correcting pipette position, moving microscope by {zdistleft} um")
-            self.microscope.relative_move(-self.config.cell_distance)
-            self.microscope.wait_until_still()
-            self.calibrated_unit.center_pipette()
-            self.calibrated_unit.wait_until_still()
-            self.microscope.relative_move(self.config.cell_distance)
-            self.microscope.wait_until_still()
+        # if self.config.cell_type == "Plate": 
+        #     self.info("centering on cell")
+        #     self.calibrated_stage.center_on_cell(cell)
+        #     self.calibrated_stage.wait_until_still()
+        #     self.info(f"correcting pipette position, moving microscope by {zdistleft} um")
+        #     self.microscope.relative_move(-self.config.cell_distance)
+        #     self.microscope.wait_until_still()
+        #     self.calibrated_unit.center_pipette()
+        #     self.calibrated_unit.wait_until_still()
+        #     self.microscope.relative_move(self.config.cell_distance)
+        #     self.microscope.wait_until_still()
         self.info("Located Cell")
 
         self.amplifier.start_patch()
@@ -397,6 +397,7 @@ class AutoPatcher(TaskController):
             self.config.cell_R_increase = 0.200
 
         while not self._isCellDetected(lastResDeque=lastResDeque,cellThreshold = self.config.cell_R_increase) and self.abort_requested == False:
+            self.pressure.clamp()
             curr_pos = self.calibrated_unit.position()
             if abs(curr_pos[2] - start_pos[2]) >= (int(self.config.max_distance)):
                 # we have moved expected um down and still no cell detected
@@ -429,7 +430,7 @@ class AutoPatcher(TaskController):
             self.sleep(1)
             self.pressure.set_pressure(100)
             self.sleep(1)
-            self.move_group_up(20)
+            self.move_group_up(-20)
             self.sleep(1)
             self.pressure.set_pressure(200)
             self.sleep(1)
