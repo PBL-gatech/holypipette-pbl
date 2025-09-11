796d700bd2951ed7858d58f03471241fc4518aa6
tiger23456 <144534146+tiger23456@users.noreply.github.com>
2025-09-11 14:11:03 -0400

    Hardware installation.
    implemented Pressure box and DAQ. all hardware functional!
    
    Co-Authored-By: Benjamin Magondu <135537119+bmagondu3@users.noreply.github.com>
    Co-Authored-By: PBL-gatech <167556129+PBL-gatech@users.noreply.github.com>

--- patch_gui.py
+++ patch_gui.py
@@ -23,7 +23,7 @@ from holypipette.gui.patch import PatchGui
 
 
 
-from rig_setup.setup_Moscow_rig import *  
+from rig_setup.setup_Bo_rig import *  
 # from rig_setup.setup_fake_rig import * 
 # from rig_setup.setup_Moscow_rig_camera import *    
 # from rig_setup.setup_Moscow_Pressuretest import *
---
+++ rig_setup/setup_Bo_rig.py
@@ -0,0 +1,48 @@
+'''
+This script sets up the Moscow rig. It is used to set up the devices and their connections. The devices are then used in the main script.
+'''
+import serial
+from holypipette.devices.amplifier.amplifier import FakeAmplifier
+from holypipette.devices.amplifier.multiclamp import MultiClampChannel
+from holypipette.devices.amplifier.DAQ import FakeDAQ, NiDAQ
+from holypipette.devices.camera.pcocamera import PcoCamera
+from holypipette.devices.pressurecontroller import MoscowPressureController, FakePressureController
+from holypipette.devices.manipulator import *
+from holypipette.devices.cellsorter import FakeCellSorterController, FakeCellSorterManip
+
+
+# set up Camera
+camera = PcoCamera()
+
+# set up Pressure Controller
+pressureControllerSerial = serial.Serial(port='COM7', baudrate=9600, timeout=0)
+pressureReaderSerial = serial.Serial(port='COM8', baudrate=9600, timeout=0)
+pressure = MoscowPressureController(channel=1, controllerSerial=pressureControllerSerial, readerSerial=pressureReaderSerial)
+#pressure = FakePressureController()
+
+# set up Ephys
+amplifier = MultiClampChannel(channel=1)
+daq = NiDAQ('Dev3', 'ai1', 'Dev3', 'ao0', 'Dev3', 'ai3')
+# daq = FakeDAQ()
+# amplifier = FakeAmplifier()
+
+# set up movement controllers
+
+controllerSerial = serial.Serial('COM9')
+controller = ScientificaSerialNoEncoder(controllerSerial)
+microscope = Microscope(controller, 3)
+microscope.up_direction = 1.0
+
+pipetteSerial = serial.Serial('COM14')
+pipetteManip = ScientificaSerialNoEncoder(pipetteSerial)
+stage = ManipulatorUnit(controller, [1, 2])
+unit = ManipulatorUnit(pipetteManip, [1, 2, 3])
+
+# set up cell sorter
+cellSorterController = FakeCellSorterController()
+cellSorterManip = FakeCellSorterManip()
+
+
+
+
+
